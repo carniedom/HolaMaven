33649d6cb3fe9d1873f750422d059f3f7be39938
carniedom <carniedom@alu.edu.gva.es>
2023-03-12 12:24:15 +0100

    Se crea el test
    HolaMonTest.java creado y primera ejecución

---
+++ Holamon/src/test/java/net/alumnado/dam/HolaMonTest.java
@@ -0,0 +1,33 @@
+package net.alumnado.dam;
+
+import net.alumnado.dam.HolaMon;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterAll;
+
+class HolaMonTest {
+
+    @Test
+    void esMayor() {
+    }
+
+    private static HolaMon helloWorld;
+
+
+    @BeforeAll
+    public static void Before() {
+        System.out.println("Antes de todas");
+    }
+
+    @Test
+    public void isGreaterTest() {
+
+        helloWorld = new HolaMon();
+        System.out.println("La Prueba");
+        if (helloWorld.esMayor(4, 3)) {
+            System.out.println("El 1 es mayor que el 2");
+        } else {
+            System.out.println("El 2 es mayor que el 1");
+        }
+    }
+}
